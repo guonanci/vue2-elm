c8a4511a7c116a1e8a28ce09b0801ed050f36bc1
guonanci <yiminanci@gmail.com>
2018-08-14 23:12:29 +0800

    utils.js

---
+++ build/utils.js
@@ -0,0 +1,14 @@
+var path = require('path')
+var config = require('../config')
+var MiniCSSExtractPlugin = require('mini-css-extract-plugin')
+
+exports.assetsPlugin = function (_path) {
+  var assetsSubDirectory = process.env.NODE_NEV === 'production' ? config.build.assetsSubDirectory : config.dev.assetsSubDirectory
+  return path.posix.join(assetsSubDirectory, _path)
+}
+
+exports.cssLoaders = function (options) {
+  options = options || {}
+
+  // generate loader string to be used with mini css extract plugin
+}
