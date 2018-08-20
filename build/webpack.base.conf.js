31550d053832e66737230ed55a080bf40213fddb
guonanci <yiminanci@gmail.com>
2018-08-20 16:43:57 +0800

    webpack.base.conf.js

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -1,3 +1,9 @@
 var path = require('path')
 var config = require('../config')
 var utils = require('../utils')
+var projectRoot = pathresolve(__dirname, '../')
+
+var env = process.env.NODE_ENV
+// check env & config/index.js to decide whether to enable CSS Sourcemaps for
+// the various precessor loaders added to vue-loader at the end of this file
+var cssSourceMapDev = (env === 'development' && config.dev.cssSourceMap)
