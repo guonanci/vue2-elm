a354f5a3d82a15cfd000cb792e5f457b6bd9cf64
guonanci <yiminanci@gmail.com>
2018-10-07 22:21:37 +0800

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,12 +15,12 @@ module.exports = {
   ],
   rules: {
     camelcase: 0,
-    comma-dangle: [2, {
+    'comma-dangle': [2, {
       'objects': 'only-multiline',
       'arrays': 'only-multiline',
     }],
     semi: [2, 'never'],
-    no-console: process.env.NODE_ENV === 'production' ? 2 : 0,
-    no-debugger: process.env.NODE_ENV === 'production' ? 2 : 0,
+    'no-console': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
   }
 }
